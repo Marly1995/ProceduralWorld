6e0e59eb03a9ad24da5d6e171cf68cba5d706e70
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2017-02-23 15:12:35 +0000

    basic mesh terrain generator

---
+++ Procedural_World/Assets/Editor/IslandGenerationEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof  (IslandGenerator))]
+public class IslandGenerationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        IslandGenerator islandGen = (IslandGenerator)target;
+
+        if(DrawDefaultInspector())
+        {
+            if(islandGen.autoUpdate)
+            {
+                islandGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button ("Generate"))
+        {
+            islandGen.GenerateMap();
+        }
+    }
+}
---
+++ Procedural_World/Assets/Scripts/NoiseGeneration.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NoiseGeneration
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int sheets, float persistance, float lacunarity, Vector2 offset)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random rndGen = new System.Random(seed);
+
+        Vector2[] sheetOffsets = new Vector2[sheets];
+        for (int i = 0; i < sheets; i++)
+        {
+            float offsetX = rndGen.Next(-100000, 100000) + offset.x;
+            float offsetY = rndGen.Next(-100000, 100000) + offset.y;
+            sheetOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2.0f;
+        float halfHeight = mapHeight / 2.0f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float amplitude = 1.0f;
+                float frequency = 1.0f;
+                float noiseHeight = 0.0f;
+
+                for (int i = 0; i < sheets; i++)
+                {
+                    float sampleX = x / scale * frequency + sheetOffsets[i].x;
+                    float sampleY = y / scale * frequency + sheetOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+        return noiseMap;
+    }
+}
