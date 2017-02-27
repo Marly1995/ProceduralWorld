3ab3f451705401b8a83aee430bad67a0ce6a50ad
marlon <14471455@students.lincoln.ac.uk>
2017-02-27 11:37:31 +0000

    plane creation new version

---
+++ Assets/Scripts/sphereMapping.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class sphereMapping : MonoBehaviour
+{
+    public int xSize, ySize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "ProceduralMesh";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        int i = 0;
+        for (int y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = new Vector4(1.0f, 0.0f, 0.0f, -1.0f);
+                i++;
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+        int[] triangles = new int[6*xSize*ySize];
+        for (int vert = 0, tri = 0, y = 0; y < ySize; y++, tri++)
+        {
+            for (int x = 0; x < xSize; x++, vert += 6, tri++)
+            {
+                triangles[vert] = tri;
+                triangles[vert + 1] = tri + xSize + 1;
+                triangles[vert + 2] = tri + 1;
+                triangles[vert + 3] = tri + 1;
+                triangles[vert + 4] = tri + xSize + 1;
+                triangles[vert + 5] = tri + xSize + 2;             
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(vertices == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
