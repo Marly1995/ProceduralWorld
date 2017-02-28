45ef8ccc182842ba8bcbc24e752bd89c492f723e
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2017-02-28 16:36:51 +0000

    sphere warped polys

--- Assets/Scripts/sphereMapping.cs
+++ Assets/Scripts/sphereMapping.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
 public class sphereMapping : MonoBehaviour
 {
-    public int xSize, ySize, zSize;
-    public int roundness;
+    public int gridSize;
+    [Range (0, 100)]
+    public float radius;
+
     private Vector3[] vertices;
     private Vector3[] normals;
     private Mesh mesh;
@@ -19,7 +21,7 @@ public class sphereMapping : MonoBehaviour
     private void Generate()
     {     
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-        mesh.name = "ProceduralMesh";
+        mesh.name = "ProceduralPlanet";
         GenerateVerts();
         GenerateTris();
     }
@@ -27,45 +29,45 @@ public class sphereMapping : MonoBehaviour
     private void GenerateVerts()
     {
         int cornerVerts = 8;
-        int edgeVerts = (xSize + ySize + zSize - 3) * 4;
+        int edgeVerts = (gridSize + gridSize + gridSize - 3) * 4;
         int faceVerts = (
-            (xSize - 1) * (ySize - 1) +
-            (xSize - 1) * (zSize - 1) +
-            (ySize - 1) * (zSize - 1)) * 2;
+            (gridSize - 1) * (gridSize - 1) +
+            (gridSize - 1) * (gridSize - 1) +
+            (gridSize - 1) * (gridSize - 1)) * 2;
         vertices = new Vector3[cornerVerts + edgeVerts + faceVerts];
         normals = new Vector3[vertices.Length];
         int i = 0;
         // rings 
-        for (int y = 0; y <= ySize; y++)
+        for (int y = 0; y <= gridSize; y++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x <= gridSize; x++)
             {
                 SetVertex(i++, x, y, 0);
             }
-            for (int z = 1; z <= zSize; z++)
+            for (int z = 1; z <= gridSize; z++)
             {
-                SetVertex(i++, xSize, y, z);
+                SetVertex(i++, gridSize, y, z);
             }
-            for (int x = xSize - 1; x >= 0; x--)
+            for (int x = gridSize - 1; x >= 0; x--)
             {
-                SetVertex(i++, x, y, zSize);
+                SetVertex(i++, x, y, gridSize);
             }
-            for (int z = zSize - 1; z > 0; z--)
+            for (int z = gridSize - 1; z > 0; z--)
             {
                 SetVertex(i++, 0, y, z);
             }
         }
         // holes
-        for (int z = 1; z < zSize; z++)
+        for (int z = 1; z < gridSize; z++)
         {
-            for (int x = 1; x < xSize; x++)
+            for (int x = 1; x < gridSize; x++)
             {
-                SetVertex(i++, x, ySize, z);
+                SetVertex(i++, x, gridSize, z);
             }
         }
-        for (int z = 1; z < zSize; z++)
+        for (int z = 1; z < gridSize; z++)
         {
-            for (int x = 1; x < xSize; x++)
+            for (int x = 1; x < gridSize; x++)
             {
                 SetVertex(i++, x, 0, z);
             }
@@ -76,60 +78,34 @@ public class sphereMapping : MonoBehaviour
 
     private void SetVertex(int i, int x, int y, int z)
     {
-        Vector3 inner = vertices[i] = new Vector3(x, y, z);
-
-        if (x < roundness)
-        {
-            inner.x = roundness;
-        }
-        else if (x > xSize - roundness)
-        {
-            inner.x = xSize - roundness;
-        }
-        if (y < roundness)
-        {
-            inner.y = roundness;
-        }
-        else if (y > ySize - roundness)
-        {
-            inner.y = ySize - roundness;
-        }
-        if (z < roundness)
-        {
-            inner.z = roundness;
-        }
-        else if (z > zSize - roundness)
-        {
-            inner.z = zSize - roundness;
-        }
-
-        normals[i] = (vertices[i] - inner).normalized;
-        vertices[i] = inner + normals[i] * roundness;
+        Vector3 v = new Vector3(x, y, z) * 2.0f/gridSize - Vector3.one;
+        normals[i] = v.normalized;
+        vertices[i] = normals[i] * radius;
     }
 
     private void GenerateTris()
     {
-        int[] trianglesZ = new int[(xSize * ySize) * 12];
-        int[] trianglesX = new int[(ySize * zSize) * 12];
-        int[] trianglesY = new int[(xSize * zSize) * 12];
-        int ring = (xSize + zSize) * 2;
+        int[] trianglesZ = new int[(gridSize * gridSize) * 12];
+        int[] trianglesX = new int[(gridSize * gridSize) * 12];
+        int[] trianglesY = new int[(gridSize * gridSize) * 12];
+        int ring = (gridSize + gridSize) * 2;
         int tZ = 0, tX = 0, tY = 0, v = 0;
 
-        for (int y = 0; y < ySize; y++, v++)
+        for (int y = 0; y < gridSize; y++, v++)
         {
-            for (int q = 0; q < xSize; q++, v++)
+            for (int q = 0; q < gridSize; q++, v++)
             {
                 tZ = MakeQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < zSize; q++, v++)
+            for (int q = 0; q < gridSize; q++, v++)
             {
                 tX = MakeQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < xSize; q++, v++)
+            for (int q = 0; q < gridSize; q++, v++)
             {
                 tZ = MakeQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < zSize - 1; q++, v++)
+            for (int q = 0; q < gridSize - 1; q++, v++)
             {
                 tX = MakeQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
             }
@@ -147,32 +123,32 @@ public class sphereMapping : MonoBehaviour
 
     private int CreateBoxTop(int[] triangles, int t, int ring)
     {
-        int v = ring * ySize;
-        for (int x = 0; x < xSize - 1; x++, v++)
+        int v = ring * gridSize;
+        for (int x = 0; x < gridSize - 1; x++, v++)
         {
             t = MakeQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
         }
         t = MakeQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
 
-        int vMin = ring * (ySize + 1) - 1;
+        int vMin = ring * (gridSize + 1) - 1;
         int vMid = vMin + 1;
         int vMax = v + 2;
 
-        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++)
+        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++)
         {
-            t = MakeQuad(triangles, t, vMin, vMid, vMin - 1, vMid + xSize - 1);
-            for (int x = 1; x < xSize - 1; x++, vMid++)
+            t = MakeQuad(triangles, t, vMin, vMid, vMin - 1, vMid + gridSize - 1);
+            for (int x = 1; x < gridSize - 1; x++, vMid++)
             {
                 t = MakeQuad(
                     triangles, t,
-                    vMid, vMid + 1, vMid + xSize - 1, vMid + xSize);
+                    vMid, vMid + 1, vMid + gridSize - 1, vMid + gridSize);
             }
-            t = MakeQuad(triangles, t, vMid, vMax, vMid + xSize - 1, vMax + 1);
+            t = MakeQuad(triangles, t, vMid, vMax, vMid + gridSize - 1, vMax + 1);
         }
 
         int vTop = vMin - 2;
         t = MakeQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
-        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++)
+        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++)
         {
             t = MakeQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
         }
@@ -184,33 +160,33 @@ public class sphereMapping : MonoBehaviour
     private int CreateBoxBot(int[] triangles, int t, int ring)
     {
         int v = 1;
-        int vMid = vertices.Length - (xSize - 1) * (zSize - 1);
+        int vMid = vertices.Length - (gridSize - 1) * (gridSize - 1);
         t = MakeQuad(triangles, t, ring - 1, vMid, 0, 1);
-        for (int x = 1; x < xSize - 1; x++, v++, vMid++)
+        for (int x = 1; x < gridSize - 1; x++, v++, vMid++)
         {
             t = MakeQuad(triangles, t, vMid, vMid + 1, v, v + 1);
         }
         t = MakeQuad(triangles, t, vMid, v + 2, v, v + 1);
 
         int vMin = ring - 2;
-        vMid -= xSize - 2;
+        vMid -= gridSize - 2;
         int vMax = v + 2;
 
-        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++)
+        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++)
         {
-            t = MakeQuad(triangles, t, vMin, vMid + xSize - 1, vMin + 1, vMid);
-            for (int x = 1; x < xSize - 1; x++, vMid++)
+            t = MakeQuad(triangles, t, vMin, vMid + gridSize - 1, vMin + 1, vMid);
+            for (int x = 1; x < gridSize - 1; x++, vMid++)
             {
                 t = MakeQuad(
                     triangles, t,
-                    vMid + xSize - 1, vMid + xSize, vMid, vMid + 1);
+                    vMid + gridSize - 1, vMid + gridSize, vMid, vMid + 1);
             }
-            t = MakeQuad(triangles, t, vMid + xSize - 1, vMax + 1, vMid, vMax);
+            t = MakeQuad(triangles, t, vMid + gridSize - 1, vMax + 1, vMid, vMax);
         }
 
         int vTop = vMin - 1;
         t = MakeQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
-        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++)
+        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++)
         {
             t = MakeQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
         }
