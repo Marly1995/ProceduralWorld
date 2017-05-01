dd5d4c17d45216934ba05bf13fbbda60e01d6d14
marlon gilliam <14471455@students.lincoln.ac.uk>
2017-05-01 14:55:45 +0100

    2 types of pine tree generation

--- Assets/Scripts/PopulatWorld.cs
+++ Assets/Scripts/PopulatWorld.cs
@@ -7,7 +7,16 @@ public class PopulatWorld : MonoBehaviour {
 	public SegmentData[] worldData;
 	public int gridSize;
 
-	public GameObject tree;
+	public GameObject pineTree1;
+    public GameObject pineTree2;
+
+    public GameObject forestTree1;
+    public GameObject forestTree2;
+    public GameObject forestTree3;
+
+    public GameObject palmTree1;
+    public GameObject palmTree2;
+
 	// Use this for initialization
 	public void Populate (SegmentData[] segments) {
 		worldData = segments;
@@ -24,26 +33,42 @@ public class PopulatWorld : MonoBehaviour {
 		for (int i = 0; i < worldData.Length; i++) 
 		{
 			int index = 0;
-			int countdown = 0;
+			int pineTreeCountdown = 0;
 			for (int x = 0; x < gridSize; x++) 
 			{
 				for (int y = 0; y < gridSize; y++) 
 				{
 					Vector3 position = worldData [i].mesh.vertices [worldData [i].mesh.triangles [index * 6]];
-					//Debug.Log (position.magnitude);
+                    float height = position.magnitude;
 
-					if (position.magnitude >= 101.0f &&
-					    position.magnitude <= 101.4f) {
-						if (countdown <= 0) {
-							GameObject obj = Instantiate (tree, position, Quaternion.identity);
-							obj.transform.rotation =  Quaternion.FromToRotation(Vector3.down, position.normalized);
-							countdown = (int)Random.Range (2.0f, 6.0f);
-						}
-						countdown--;
+
+                    if (height >= 101.0f &&
+					    height <= 101.4f) {
+                        SpawnPineTree(position, pineTreeCountdown);
 					}
 					index++;
 				}
 			}
 		}
 	}
+
+    void SpawnPineTree(Vector3 position, int pineTreeCountdown)
+    {
+        if (pineTreeCountdown <= 0)
+        {
+            if(Random.Range(0.0f, 2.0f) >= 1.0f)
+            {
+                GameObject obj = Instantiate(pineTree1, position, Quaternion.identity);
+                obj.transform.rotation = Quaternion.FromToRotation(Vector3.down, position.normalized);
+                pineTreeCountdown = (int)Random.Range(2.0f, 6.0f);
+            }
+            else
+            {
+                GameObject obj = Instantiate(pineTree2, position, Quaternion.identity);
+                obj.transform.rotation = Quaternion.FromToRotation(Vector3.down, position.normalized);
+                pineTreeCountdown = (int)Random.Range(2.0f, 6.0f);
+            }
+        }
+        pineTreeCountdown--;
+    }
 }
