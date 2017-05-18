0a11b3fe31b54502762350c2896a2de36d055216
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2017-05-18 16:11:46 +0100

    added tastey buttons

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour {
+
+    public Button random;
+    public Button randomColor;
+
+    public IslandGenerator gen;
+
+	// Use this for initialization
+	void Start () {
+        random.onClick.AddListener(GenerateRandom);
+        randomColor.onClick.AddListener(GenerateRandomColors);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateRandom()
+    {
+        gen.RandomizeColors();
+        gen.randomize = true;
+        gen.DrawMapInEditor();
+        gen.randomize = false;
+    }
+
+    void GenerateRandomColors()
+    {
+        gen.randomize = false;
+        gen.RandomizeColors();
+        gen.DrawMapInEditor();
+    }
+}
